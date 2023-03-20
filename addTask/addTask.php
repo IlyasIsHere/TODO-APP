5b61db87bd20c32d930522c8f0dc2e28afe6b496
IlyasIsHere <ilyashk5@gmail.com>
2023-03-20 23:32:23 +0000

    Page for adding a task

--- addTask/addTask.php
+++ addTask/addTask.php
@@ -1,3 +1,6 @@
+<?php
+require_once '../include/pdo.php';
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -9,27 +12,43 @@
     <link rel="stylesheet" href="addTaskStyle.css">
 </head>
 <body>
-    <video autoplay loop playsinline muted>
-        <source src="../Login/Pexels%20Videos%204703.mp4">
-    </video>
-    <div class="addTask-card">
-        <h2>Login</h2>
-        <form method="post">
-            <div class="form-container">
-                <div class="form-row">
-                    <label for="username">Username</label>
-                    <input type="text" id="username">
-                </div>
-                <div class="form-row">
-                    <label for="password">Password</label>
-                    <input type="password" id="password">
-                </div>
-                <div class="form-row">
-                    <input type="submit">
-                </div>
+<video autoplay loop playsinline muted>
+    <source src="../Login/Pexels%20Videos%204703.mp4">
+</video>
+<div class="addTask-card">
+    <h2>Add a Task</h2>
+    <form method="post">
+        <div class="form-container">
+            <div class="form-row">
+                <label for="task-name">Task Name</label>
+                <input type="text" id="task-name" required>
             </div>
-        </form>
+            <div class="form-row">
+                <label for="task-category">Task Category</label>
+                <select>
+                    <?php
+                    $query1 = 'SELECT * FROM categories;';
+                    $stmt = $pdo->query($query1);
+                    while ($row = $stmt->fetch()) {
+                        echo "<option value='".$row['category_id']."'>".$row['category_name']."</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="form-row">
+                <label for="task-description">Description</label>
+                <textarea id="task-description"></textarea>
+            </div>
+            <div class="form-row">
+                <label for="duedate">Due Date</label>
+                <input type="date" id="duedate" required>
+            </div>
+            <div class="form-row">
+                <input type="submit" value="Add">
+            </div>
+        </div>
+    </form>
 
-    </div>
+</div>
 </body>
 </html>
