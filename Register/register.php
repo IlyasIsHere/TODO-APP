421dd1efb0f23c51972c1bd5d111c32caad6dfc4
unknown <a.baidoune01@gmail.com>
2023-03-20 21:33:02 +0000

    register added

--- Register/register.php
+++ Register/register.php
@@ -1,3 +1,20 @@
 <?php
 require_once '../include/pdo.php';
+?>
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="registerStyle.css">
+</head>
+<body>
+    <video autoplay loop playsinline muted>
+        <source src="../Login/Pexels%20Videos%204703.mp4">
+    </video>
+</body>
+</html>
