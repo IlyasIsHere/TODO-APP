c30eb152ddb9e09f73310f8cfb8f7fc311da53cf
IlyasIsHere <ilyashk5@gmail.com>
2023-03-21 20:49:06 +0000

    Adding a task almost finished

--- addTask/addTask.php
+++ addTask/addTask.php
@@ -17,31 +17,31 @@ require_once '../include/pdo.php';
 </video>
 <div class="addTask-card">
     <h2>Add a Task</h2>
-    <form method="post">
+    <form method="post" action="">
         <div class="form-container">
             <div class="form-row">
                 <label for="task-name">Task Name</label>
-                <input type="text" id="task-name" required>
+                <input type="text" id="task-name" name="name" required>
             </div>
             <div class="form-row">
                 <label for="task-category">Task Category</label>
-                <select>
+                <select name="category" id="task-category">
                     <?php
                     $query1 = 'SELECT * FROM categories;';
                     $stmt = $pdo->query($query1);
                     while ($row = $stmt->fetch()) {
-                        echo "<option value='".$row['category_id']."'>".$row['category_name']."</option>";
+                        echo "<option value='" . $row['category_id'] . "'>" . $row['category_name'] . "</option>";
                     }
                     ?>
                 </select>
             </div>
             <div class="form-row">
                 <label for="task-description">Description</label>
-                <textarea id="task-description"></textarea>
+                <textarea id="task-description" name="description"></textarea>
             </div>
             <div class="form-row">
                 <label for="duedate">Due Date</label>
-                <input type="date" id="duedate" required>
+                <input type="date" id="duedate" name="date" required>
             </div>
             <div class="form-row">
                 <input type="submit" value="Add">
@@ -50,5 +50,19 @@ require_once '../include/pdo.php';
     </form>
 
 </div>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $stmt = $pdo->prepare("INSERT INTO tasks (task_name, task_description, due_date, task_status) VALUES (?, ?, ?, 'in progress')");
+    $name = $_POST["name"];
+    $desc = $_POST["description"];
+    $date = $_POST["date"];
+    if ($stmt->execute([$name, $desc, $date])) {
+        ?>
+<div class="success">Task added!</div>
+<?php
+    }
+}
+?>
+
 </body>
 </html>
