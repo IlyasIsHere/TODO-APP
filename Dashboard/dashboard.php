917c9cab71bb0b76530a42b89e42a17b3f45611a
unknown <a.baidoune01@gmail.com>
2023-03-21 21:51:28 +0000

    dashboard ini

---
+++ Dashboard/dashboard.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="loginStyle.css">
+</head>
+<body>
+    <video autoplay loop playsinline muted>
+        <source src="Pexels%20Videos%204703.mp4">
+    </video>
+    
+</body>
+</html>
